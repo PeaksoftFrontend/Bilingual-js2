67399effb9b032c62ecf3585d0e39cebb186494c
Mederkun <shanyzakovmeder3@gmail.com>
2024-09-11 22:33:25 +0300

    typography added

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
 import "./index.css";
+import theme from "./theme/theme.js";
+import { ThemeProvider } from "@emotion/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
+   <ThemeProvider theme={theme}>
     <App />
+   </ThemeProvider>
   </StrictMode>
 );
