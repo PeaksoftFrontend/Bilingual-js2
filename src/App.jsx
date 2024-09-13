28a81d294feb4ebf3cc3d1cd09195f0001d01e74
Айдана Пазылбекова <ajdanapazylbekova52@gmail.com>
2024-09-13 19:55:02 +0300

    cleared  App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import { Button } from "./components/UI/Button";
-import DeleteIcon from "@mui/icons-material/Delete";
-
 export const App = () => {
   return (
     <div>
       <h1>Bilingual</h1>
-      <Button variant="contained" icon={<DeleteIcon />}>
-        delete
-      </Button>
     </div>
   );
 };
