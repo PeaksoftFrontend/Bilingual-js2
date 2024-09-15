6c95d0a66f6ebc8a94abd84b8f5a09cefdfbc11f
Mederkun <shanyzakovmeder3@gmail.com>
2024-09-16 00:19:31 +0300

    icons finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 export const App = () => {
+  var s = "ee";
+  console.log(s);
   return (
     <div>
       <h1>Bilingual</h1>
---
+++ src/assets/icons/index.js
@@ -0,0 +1,62 @@
+// Importing logos from the 'logo' folder
+import Logo from "./logo/logo.svg";
+import MinorLogo from "./logo/minor_logo.svg";
+import CheckSquare1 from "./icon/check-square-1.svg";
+import CheckSquare from "./icon/check-square.svg";
+import PlayPause from "./icon/Component 149.svg";
+import PlayPauseMinor from "./icon/Component 181.svg";
+import EyeOff from "./icon/eye-off.svg";
+import Eye from "./icon/eye.svg";
+import Sound from "./icon/Group 13.svg";
+import SoundBlueXL from "./icon/Group 14.svg";
+import Tick from "./icon/Group 1414.svg";
+import xIcon from "./icon/Group 1415.svg";
+import Trash from "./icon/Group 1422.svg";
+import NoteGreySmall from "./icon/Group 1423.svg";
+import xGreyIcon from "./icon/Group 1425.svg";
+import RedTrash from "./icon/Group 1426.svg";
+import Note from "./icon/Group 1427.svg";
+import Sound10 from "./icon/Group 1441.svg";
+import Sound11 from "./icon/Group 1442.svg";
+import Hover from "./icon/on hover.svg";
+import Play from "./icon/play 1.svg";
+import Usual from "./icon/usual.svg";
+import VectorIcon from "./icon/Vector.svg";
+import CheckBold from "./check_radio_switch/Component148.svg";
+import SigninCheck from "./check_radio_switch/Component182.svg";
+import SwitchOn from "./check_radio_switch/Group1382.svg";
+import SwitchOff from "./check_radio_switch/Group 1380.svg";
+import Radio from "./check_radio_switch/x.svg";
+
+export const Icons = {
+  SwitchOn,
+  Radio,
+  SwitchOff,
+  SigninCheck,
+  CheckBold,
+  Logo,
+  MinorLogo,
+  CheckSquare1,
+  CheckSquare,
+  PlayPause,
+  PlayPauseMinor,
+  EyeOff,
+  Eye,
+  Sound,
+  SoundBlueXL,
+  Tick,
+  xIcon,
+  Trash,
+  NoteGreySmall,
+  xGreyIcon,
+  RedTrash,
+  Note,
+  Sound10,
+  Sound11,
+  Hover,
+  Play,
+  Usual,
+  VectorIcon,
+};
+
+export default Icons;
