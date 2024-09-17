20421e355cac14c71ba9786ceed3f5f7e64c8442
Mederkun <shanyzakovmeder3@gmail.com>
2024-09-17 19:55:55 +0300

    updated

--- src/components/InfoCards/Information.jsx
+++ src/components/InfoCards/Information.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Grid, Typography, CardContent, Box } from "@mui/material";
 import { styled } from "@mui/system";
 import ImageInfoOne from "../../assets/images/info_image_1.svg";
@@ -7,54 +6,59 @@ import ImageInfoThree from "../../assets/images/info_image_3.svg";
 
 export const InfoCards = () => {
   return (
-    <StyledGrid
-      container
-      direction="row"
-      justifyContent="center"
-      alignItems="center"
-      spacing={15}
-    >
-      <Grid item>
-        <StyledBox>
-          <ImageInfoOne />
-          <CardContent>
-            <Typography variant="body1Bold" align="center">
-              Over 10,000 fee waivers for the Bilingual English Test are offered
-              annually.
-            </Typography>
-          </CardContent>
-        </StyledBox>
-      </Grid>
+    <StyledGrid>
+      <StyledGridContainer>
+        <Grid item>
+          <StyledBox>
+            <ImageInfoOne />
+            <CardContent>
+              <Typography variant="body1Bold" align="center">
+                Over 10,000 fee waivers for the Bilingual English Test are
+                offered annually.
+              </Typography>
+            </CardContent>
+          </StyledBox>
+        </Grid>
 
-      <Grid item>
-        <StyledBox>
-          <ImageInfoTwo />
-          <CardContent>
-            <Typography variant="body1Bold" align="center">
-              Students from over 200 countries and territories have benefitted.
-            </Typography>
-          </CardContent>
-        </StyledBox>
-      </Grid>
+        <Grid item>
+          <StyledBox>
+            <ImageInfoTwo />
+            <CardContent>
+              <Typography variant="body1Bold" align="center">
+                Students from over 200 countries and territories have
+                benefitted.
+              </Typography>
+            </CardContent>
+          </StyledBox>
+        </Grid>
 
-      <Grid item>
-        <StyledBox>
-          <ImageInfoThree />
-          <CardContent>
-            <Typography variant="body1Bold" align="center">
-              Eligible students can take the test with absolutely zero cost to
-              them.
-            </Typography>
-          </CardContent>
-        </StyledBox>
-      </Grid>
+        <Grid item>
+          <StyledBox>
+            <ImageInfoThree />
+            <CardContent>
+              <Typography variant="body1Bold" align="center">
+                Eligible students can take the test with absolutely zero cost to
+                them.
+              </Typography>
+            </CardContent>
+          </StyledBox>
+        </Grid>
+      </StyledGridContainer>
     </StyledGrid>
   );
 };
 
-const StyledGrid = styled(Grid)(({ theme }) => ({
-  padding: theme.spacing(15), // 15 * 8px = 120px
+const StyledGrid = styled(Box)(({ theme }) => ({
+  padding: theme.spacing(15),
   backgroundColor: "#FEF5E8",
+}));
+
+const StyledGridContainer = styled(Grid)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: theme.spacing(15),
 }));
 
 const StyledBox = styled(Box)({
