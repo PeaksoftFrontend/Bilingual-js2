820914ec54e9e53e118f3c0fb791fe1f5deffaa2
Айдана Пазылбекова <ajdanapazylbekova52@gmail.com>
2024-09-19 18:06:03 +0300

    update font-size

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
-import { ShowSnackbar, Snackbar } from "./components/UI/snackbar/SnackBar";
-
 export const App = () => {
   return (
     <div>
-      <h1>React Toastify Snackbar Example</h1>
-      <button
-        onClick={() =>
-          ShowSnackbar("Success message!", "success", "this file succsess")
-        }
-      >
-        Show Success
-      </button>
-      <button
-        onClick={() => ShowSnackbar("Error message!", "error", "this file ")}
-      >
-        Show Error
-      </button>
-      <Snackbar />
+      <h1>Bilingual</h1>
     </div>
   );
 };
