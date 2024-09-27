4299d43f29db05857780327ec76fa40662eb4e67
Mederkun <shanyzakovmeder3@gmail.com>
2024-09-27 20:16:24 +0300

    clear app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { LearningRoadmap } from "./components/landingPageComponents/LearningRoadmap";
-
 export const App = () => {
   return (
     <div>
       <h1>Bilingual</h1>
-      <LearningRoadmap />
     </div>
   );
 };
