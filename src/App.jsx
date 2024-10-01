3d03531d8dfab9c91e8680a7d4238c2d5c4bcab0
Mederkun <shanyzakovmeder3@gmail.com>
2024-10-01 19:17:53 +0300

    cleared app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { Feedback } from "./components/landingPageComponents/Feedback";
-
 export const App = () => {
   return (
     <div>
       <h1>Bilingual</h1>
-      <Feedback />
     </div>
   );
 };
