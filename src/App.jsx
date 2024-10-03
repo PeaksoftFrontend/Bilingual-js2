75b5a6633b52ae518a8126df56f824bd831247d3
nuralim <nuralim20062002@gmail.com>
2024-10-03 21:05:14 +0300

    fixik

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { Footer } from "./components/landingPageComponents/Footer";
 export const App = () => {
   return (
     <div>
       <h1>Bilingual</h1>
-      <Footer />
     </div>
   );
 };
