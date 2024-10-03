88c2447b5b72c9309851adac649ea0127210ddef
Samirkosh <samiramira1601@gmail.com>
2024-10-03 18:34:56 +0300

    added modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,50 @@
+import { Modal } from "./components/UI/modal/Modal";
+import { useState } from "react";
+import { Header } from "./components/landingPageComponents/Header";
+import { Input } from "./components/UI/input/Input";
+import { Button } from "./components/UI/button/Button";
+import { styled } from "@mui/material";
+
 export const App = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <div>
-      <h1>Bilingual</h1>
+      <Header />
+      {open ? (
+        <Styled role="USER" handleClose={handleClose} open={open}>
+          <div>
+            <h5>Title</h5>
+            <Input placeholder={"Select the main idea"} />
+            <p>Is true option?</p>
+          </div>
+          <StyledBtn>
+            <Button onClick={handleClose}>close</Button>
+            <Button>add</Button>
+          </StyledBtn>
+        </Styled>
+      ) : (
+        <Button onClick={handleOpen}>open</Button>
+      )}
     </div>
   );
 };
+
+const Styled = styled(Modal)({
+  // display: "flex",
+  // flexDirection: "column",
+});
+
+const StyledBtn = styled("div")({
+  display: "flex",
+  justifyContent: "end",
+  gap: "16px",
+  padding: "26px 0 0 0",
+});
---
+++ src/components/UI/modal/Modal.jsx
@@ -0,0 +1,53 @@
+import { Box, Modal as MuiModal, styled } from "@mui/material";
+import { Icons } from "../../../assets/icons";
+
+export const Modal = ({ children, handleClose, open, role, ...props }) => {
+  return (
+    <StyledModal open={open} onClose={handleClose} {...props}>
+      <StyledBox role={role}>
+        {role === "ADMIN" && (
+          <StyledIcon onClick={handleClose}>
+            <Icons.XSmall />
+          </StyledIcon>
+        )}
+        <StyledChildren role={role}>{children}</StyledChildren>
+      </StyledBox>
+    </StyledModal>
+  );
+};
+
+const StyledBox = styled(Box)(({ role }) => ({
+  width: role === "ADMIN" ? "637px" : "407px",
+  height: "fit-content",
+  background:
+    role === "ADMIN"
+      ? "linear-gradient(to bottom, #FEFEFE 70%, #F0F1F1 70%)"
+      : "#FEFEFE",
+  borderRadius: "20px",
+  padding: role === "ADMIN" ? "20px 32px 0 0" : "36px 60px",
+}));
+
+const StyledModal = styled(MuiModal)(() => ({
+  width: "100%",
+  height: "100vh",
+  backgroundColor: "#655f5f28",
+  position: "fixed",
+  top: "0",
+  left: "0",
+  display: "flex",
+  justifyContent: " center",
+  alignItems: "center",
+}));
+
+const StyledIcon = styled("div")({
+  display: "flex",
+  justifyContent: "end",
+  cursor: "pointer",
+});
+
+const StyledChildren = styled("div")(({ role }) => ({
+  display: "flex",
+  flexDirection: "column",
+  gap: role === "ADMIN" ? "108px" : "0",
+  padding: role === "ADMIN" ? "40px 26px 26px 60px" : "0",
+}));
