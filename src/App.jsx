c78f0be7f7e0f8b1918c084d5a3049749f546fa4
Samirkosh <samiramira1601@gmail.com>
2024-10-12 21:21:48 +0300

    clear app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { SelectWords } from "./components/adminPage/Test Verification/SelectWords";
-
 export const App = () => {
   return (
     <div>
-      {/* <h1>Bilingual</h1> */}
-      <SelectWords />
+      <h1>Bilingual</h1>
     </div>
   );
 };
