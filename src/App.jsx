884d8e83439913e048ea2802ce167731d57207e3
zhakshylyk <zhakshylyk@macs-MacBook-Pro.local>
2024-10-19 17:55:08 +0300

    added uppload image

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { UploadImage } from "./pages/adminUploadImage/UploadImage";
+
 export const App = () => {
   return (
     <div>
       <h1>Bilingual</h1>
+      <UploadImage />
     </div>
   );
 };
---
+++ src/pages/adminUploadImage/UploadImage.jsx
@@ -0,0 +1,130 @@
+import { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { styled } from "@mui/material";
+import { Button } from "../../components/UI/button/Button";
+import { ContentWrapper } from "../../components/UI/content_wrapper/ContentWrapper";
+import { Input } from "../../components/UI/input/Input";
+
+export const UploadImage = () => {
+  const [uploadedImage, setUploadedImage] = useState(null);
+  const [fileName, setFileName] = useState("");
+  const [isDragging, setIsDragging] = useState(false);
+
+  const onDrop = useCallback((acceptedFiles) => {
+    const file = acceptedFiles[0];
+    const imageUrl = URL.createObjectURL(file);
+    setUploadedImage(imageUrl);
+    setFileName(file.name);
+    setIsDragging(false);
+  }, []);
+
+  const onDragEnter = () => {
+    setIsDragging(true);
+  };
+
+  const onDragLeave = () => {
+    setIsDragging(false);
+  };
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: "image/*",
+    multiple: false,
+    onDragEnter,
+    onDragLeave,
+  });
+
+  return (
+    <div>
+      <ContentWrapper>
+        <h1>Upload an Image</h1>
+
+        <ContainerUploadImage>
+          <div {...getRootProps()} style={{ cursor: "pointer" }}>
+            <input {...getInputProps()} />
+            {uploadedImage ? (
+              <ImagePreview src={uploadedImage} alt="Uploaded" />
+            ) : (
+              <UploadImageWrapper isDragging={isDragging}>
+                <p>Upload image</p>
+              </UploadImageWrapper>
+            )}
+          </div>
+
+          <p>{fileName ? fileName : "No file uploaded"}</p>
+        </ContainerUploadImage>
+
+        <WrapperInputAndButtons>
+          <InputLabel htmlFor="answer">
+            Correct answer
+            <Input type="text" placeholder={"write text"} id={"answer"} />
+          </InputLabel>
+
+          <WrapperButtons>
+            <StyledButton variant="outlined">Go Back</StyledButton>
+            <Button variant="success">Save</Button>
+          </WrapperButtons>
+        </WrapperInputAndButtons>
+      </ContentWrapper>
+    </div>
+  );
+};
+
+const StyledButton = styled(Button)(() => ({
+  textWrap: "nowrap",
+}));
+
+const WrapperInputAndButtons = styled("section")({
+  display: "flex",
+  flexDirection: "column",
+  gap: "32px",
+});
+
+const WrapperButtons = styled("section")({
+  display: "flex",
+  justifyContent: "end",
+  gap: "16px",
+});
+
+const UploadImageWrapper = styled("section")(({ isDragging }) => ({
+  position: "relative",
+  width: "181px",
+  height: "178px",
+  borderRadius: "8px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  color: "#3A10E5",
+  fontWeight: "900",
+  cursor: "pointer",
+  overflow: "hidden",
+  border: isDragging ? "2px dashed #2121219c" : "2px solid #D4D0D0",
+  transition: "transform 0.3s ease, background 0.3s ease",
+  background: isDragging ? "#4646463a" : "transparent",
+  "&:hover": {
+    transform: "scale(1.03)",
+    border: "2px solid #3A10E5",
+  },
+}));
+
+const ContainerUploadImage = styled("section")({
+  display: "flex",
+  alignItems: "center",
+  gap: "40px",
+});
+
+const InputLabel = styled("label")({
+  color: "#4c4859",
+  fontWeight: "600",
+  marginTop: "24px",
+  display: "flex",
+  flexDirection: "column",
+  gap: "12px",
+});
+
+const ImagePreview = styled("img")({
+  width: "181px",
+  height: "178px",
+  objectFit: "cover",
+  borderRadius: "8px",
+});
