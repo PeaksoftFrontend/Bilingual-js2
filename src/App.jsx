e278b584162e41f2caf20103e76c340c2271a2fc
zhakshylyk <zhakshylyk@macs-MacBook-Pro.local>
2024-10-19 17:55:44 +0300

    clear app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { UploadImage } from "./pages/adminUploadImage/UploadImage";
-
 export const App = () => {
   return (
     <div>
       <h1>Bilingual</h1>
-      <UploadImage />
     </div>
   );
 };
