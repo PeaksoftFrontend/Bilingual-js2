6e6f50b6be49c03b82e9269d884f3b74bbb9efb9
nuralim <nuralim20062002@gmail.com>
2024-10-24 20:33:19 +0300

    cleared the main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { VariantAudio } from "./components/adminPage/test/VariantAudio";
-
 export const App = () => {
   return (
     <div>
       <h1>Bilingual</h1>
-      <VariantAudio />
     </div>
   );
 };
