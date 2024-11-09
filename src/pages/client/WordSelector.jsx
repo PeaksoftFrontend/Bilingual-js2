4a9cdac40ffe5a70db64baec68e6411b744c7e4e
nuralim <nuralim20062002@gmail.com>
2024-11-09 18:56:34 +0300

    minor chenges words map

--- src/pages/client/WordSelector.jsx
+++ src/pages/client/WordSelector.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Box, Typography, Button, Grid } from "@mui/material";
 import { styled } from "@mui/system";
-import { words } from "../../utils/helpers/words";
+import { words } from "../../utils/constants/words";
 import { Duration } from "../../components/UI/duration/Duration";
 import { ContentWrapper } from "../../components/UI/content_wrapper/ContentWrapper";
 
