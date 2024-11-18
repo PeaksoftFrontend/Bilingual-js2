e45a3cb77d1808f80048b5da403281b186c1ae52
nuralim <nuralim20062002@gmail.com>
2024-11-19 02:37:51 +0300

    Audio-Recording-Interface

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import { theme } from "./theme/theme";
 import { ThemeProvider } from "@mui/material";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
-import { AppRoutes } from "./routes/routes";
+import { AudioRecordingInterface } from "./pages/evaluated/RecordSayingStatment";
+// import { AppRoutes } from "./routes/routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <ThemeProvider theme={theme}>
-      <AppRoutes />
+      {/* <AppRoutes /> */}
+      <AudioRecordingInterface />
     </ThemeProvider>
   </Provider>
 );
