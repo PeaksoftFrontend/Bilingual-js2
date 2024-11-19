d1b27938bd4001d00516cb135fd422f9c389f670
nuralim <nuralim20062002@gmail.com>
2024-11-19 14:32:05 +0300

    переименовал компонент

--- src/pages/evaluated/RecordSayingStatment.jsx
+++ src/pages/evaluated/RecordSayingStatment.jsx
@@ -7,7 +7,7 @@ import recording from "../../assets/images/recording.png";
 import { styled } from "@mui/material";
 import { Button } from "../../components/UI/button/Button";
 
-export const AudioRecordingInterface = () => {
+export const RecordSayingStatment = () => {
   const [isRecording, setIsRecording] = useState(false);
   const [isRecorded, setIsRecorded] = useState(false);
   const [audioUrl, setAudioUrl] = useState(null);
