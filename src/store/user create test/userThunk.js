4ec0b855a6f7e9854942d2a1fdca61641c855bec
Samirkosh <samiramira1601@gmail.com>
2024-11-29 02:14:26 +0300

    added test integration

---
+++ src/store/user create test/userThunk.js
@@ -0,0 +1,15 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { axiosInstance } from "../../config/axiosInstance";
+
+export const userTestRequest = createAsyncThunk(
+  "test/userTestRequest",
+  async (payload, { rejectWithValue }) => {
+    try {
+      const { data } = await axiosInstance.get("tests", payload);
+
+      return data;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
