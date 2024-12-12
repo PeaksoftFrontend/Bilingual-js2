8ff2d1aca0ee1d384ee5986f7d4e543b26e7a4aa
Alisher-00kg <alisher.estebesov@gmail.com>
2024-12-12 22:13:07 +0300

    first changes and create ResultPage component

--- src/pages/client/user-test/ResultPage.jsx
+++ src/pages/client/user-test/ResultPage.jsx
@@ -1,16 +1,49 @@
 import { styled } from "@mui/material";
 import { AdminTable } from "../../../components/UI/admin-table/AdminTable";
 import { ContentWrapper } from "../../../components/UI/content_wrapper/ContentWrapper";
-import {
-  RESULTDATA,
-  userResultData,
-} from "../../../utils/constants/AdminTable";
+
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getUserResults } from "../../../store/user__result-table/userTableThunk";
 
 export const ResultPage = () => {
+  const dispatch = useDispatch();
+  const { user_table } = useSelector((state) => state.user_table);
+  useEffect(() => {
+    dispatch(getUserResults());
+  }, [dispatch]);
+
+  const columns = [
+    {
+      Header: "#",
+      accessor: "id",
+    },
+    {
+      Header: "Date of Submition",
+      accessor: "dateOfSubmition",
+    },
+    {
+      Header: "Test Name",
+      accessor: "testName",
+    },
+    {
+      Header: "Status",
+      accessor: "status",
+    },
+    {
+      Header: "Score",
+      accessor: "score",
+    },
+    {
+      Header: "",
+      accessor: "icon",
+    },
+  ];
+
   return (
     <StyledAllContainer>
       <ContentWrapper>
-        <AdminTable columns={RESULTDATA} data={userResultData} />
+        <AdminTable columns={columns} data={user_table} />
       </ContentWrapper>
     </StyledAllContainer>
   );
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "./auth/authSlice";
 import { adminReducer } from "./admin create test/adminSlice";
+import { userTableReducer } from "./user__result-table/userTableSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     test: adminReducer,
+    user_table: userTableReducer,
   },
 });
